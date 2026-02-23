706c7744a4602ac39869906cd4a74f02de1bc2b0
sokpheng001 <kimchansokpheng123@gmail.com>
2026-02-23 22:32:03 +0700

    added new pattern

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,6 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
 
 public class Main {
     public static void main(String[] args) {
-        try{
-            Connection connection
-                    = DriverManager.getConnection(
-                            "",
-                    "",
-                    ""
-            );
-        }catch (Exception e){
 
-        }
     }
 }
